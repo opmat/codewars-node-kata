c9bcfab1b2573f0c736a36df640733189b76c08d
Matiluko Opeyemi Emmanuel <opmat01@yahoo.com>
2022-12-30 23:22:54 +0100

    initial test for detect_panagram

---
+++ tests/detect_pangramSpecs.js
@@ -0,0 +1,14 @@
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", () => {
+  it("test1", () => {
+    var string = "The quick brown fox jumps over the lazy dog."
+    assert.strictEqual(isPangram(string), true)
+  })
+  it("test2", () => {
+    var string = "This is not a pangram."
+    assert.strictEqual(isPangram(string), false)
+  });
+});
